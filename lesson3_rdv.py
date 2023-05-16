6edbf01baebebcfef242d34203863b662e7f8377
Dmitri Camenev <dmit.cameneff@yandex.ru>
2023-05-16 23:40:22 +0300

    Урок 3. Домашнее задание

--- lesson3_rdv.py
+++ lesson3_rdv.py
@@ -1,19 +1,20 @@
 """
 Skypro. Профессия "Python-разработчик" ПОТОК
-
 Урок 3. Списки и циклы. Домашнее задание 
 Родительский Дмитрий Вячеславович
 ***** Часть 2. Задание со звездочкой ****
-
 """
 
 # Вопросы и ответы пользователя
 questions = ["My name ___  Vova", "I ___ a coder", "I live ___ Moscow"]
 answers = ["is", "am", "in"]
 
+# Слово для старта программы
 MAGIC_WORD_TO_BEGIN = "ready"
+# Количество попыток вводом пользователем ответов
 USER_ATTEMPTS = 3
 
+# Счетчики
 # Правильные ответы пользователя
 correct_user_answers = 0
 # Количество набранных баллов
@@ -40,7 +41,7 @@ if user_choice == MAGIC_WORD_TO_BEGIN:
             total_user_score += USER_ATTEMPTS
         #Неправильный ответ
         else:
-            # Уменьшаем счетчик попыток ответа
+            # Уменьшаем счетчик попыток ответа пользователя
             attempt -= 1
             for j in range(attempt):
                 user_answer = (input(f"Осталось попыток: {attempt-j} попробуйте ещё раз >>> ")).lower().strip()
@@ -50,13 +51,12 @@ if user_choice == MAGIC_WORD_TO_BEGIN:
                     correct_user_answers += 1
                     total_user_score += (attempt-j)
                     break
-            # Правильного ответа так и нет
+            # Правильного ответа от пользователя так и не получили
             if user_answer != answers[i]: print(f"Увы, но нет. Верный ответ: {answers[i]}")
 
     print(f"Вот и всё! Вы ответили на {correct_user_answers} вопросов из {USER_ATTEMPTS} верно, вы набрали {total_user_score} баллов.")  
     
- 
+# Пользователь так и не введ нужное слово для начала теста 
 else: print("Кажется, вы не хотите играть. Очень жаль.")
-
 
 #########################
