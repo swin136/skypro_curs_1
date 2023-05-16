b8675b92573f639f3694cc764bec13d5851033ac
Dmitri Camenev <dmit.cameneff@yandex.ru>
2023-05-16 23:28:03 +0300

    Урок 3. Домашняя работа

---
+++ lesson3_rdv.py
@@ -0,0 +1,62 @@
+"""
+Skypro. Профессия "Python-разработчик" ПОТОК
+
+Урок 3. Списки и циклы. Домашнее задание 
+Родительский Дмитрий Вячеславович
+***** Часть 2. Задание со звездочкой ****
+
+"""
+
+# Вопросы и ответы пользователя
+questions = ["My name ___  Vova", "I ___ a coder", "I live ___ Moscow"]
+answers = ["is", "am", "in"]
+
+MAGIC_WORD_TO_BEGIN = "ready"
+USER_ATTEMPTS = 3
+
+# Правильные ответы пользователя
+correct_user_answers = 0
+# Количество набранных баллов
+total_user_score = 0
+
+# Начало работы программы
+
+# НЕ допускаем ввода "пустого" ответа пользователем
+user_choice = ""
+while user_choice == "":
+    user_choice = (input(f"Привет! Предлагаю проверить свои знания английского! Наберите \"{MAGIC_WORD_TO_BEGIN}\", чтобы начать! ")).lower().strip()
+
+if user_choice == MAGIC_WORD_TO_BEGIN:
+    # Начинаем наш цикл вопросов 
+    for i in range(len(questions)):
+        print(f"Вопрос № {i + 1}")
+        print(questions[i])
+        attempt = USER_ATTEMPTS
+        # Считываем ответ пользователя
+        user_answer = (input("Ваш ответ: ")).lower().strip()
+        if user_answer == answers[i]:
+            print("Ответ верный!")
+            correct_user_answers += 1
+            total_user_score += USER_ATTEMPTS
+        #Неправильный ответ
+        else:
+            # Уменьшаем счетчик попыток ответа
+            attempt -= 1
+            for j in range(attempt):
+                user_answer = (input(f"Осталось попыток: {attempt-j} попробуйте ещё раз >>> ")).lower().strip()
+                if user_answer == answers[i]:
+                    # Мы получили правильный ответ
+                    print("Ответ верный!")
+                    correct_user_answers += 1
+                    total_user_score += (attempt-j)
+                    break
+            # Правильного ответа так и нет
+            if user_answer != answers[i]: print(f"Увы, но нет. Верный ответ: {answers[i]}")
+
+    print(f"Вот и всё! Вы ответили на {correct_user_answers} вопросов из {USER_ATTEMPTS} верно, вы набрали {total_user_score} баллов.")  
+    
+ 
+else: print("Кажется, вы не хотите играть. Очень жаль.")
+
+
+#########################
